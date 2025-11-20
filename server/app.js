93a31a4f50cda661344a0fa735892d06e7c16164
peer hammad <peerhammad007@gmail.com>
2025-11-20 20:36:57 +0530

    fixed the CORS bug

--- server/app.js
+++ server/app.js
@@ -13,9 +13,8 @@ const allowedOrigins = [
 app.use(cors({
     origin: (origin, callback) => {
         if (!origin) return callback(null, true);
-        if (allowedOrigins.includes(origin)) {
-            return callback(new Error('CORS blocked'));
-        }
+        if (allowedOrigins.includes(origin)) return callback(null, true);
+        return callback(new Error('CORS blocked'));
     }
 }));
 app.use(express.json());
