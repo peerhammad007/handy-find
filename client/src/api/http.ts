e26c7c8bbcfe0c779ac23c76339acbebb5fd83a4
peer hammad <peerhammad007@gmail.com>
2026-01-02 18:25:59 +0530

    resolved bug in http.ts

--- client/src/api/http.ts
+++ client/src/api/http.ts
@@ -1,7 +1,6 @@
 import axios from "axios";
 import { API_BASE_URL } from "../config/apiConfig";
 
-// Create Axios instance
 const http = axios.create({
     baseURL: API_BASE_URL,
     headers: {
@@ -9,7 +8,6 @@ const http = axios.create({
     },
 });
 
-// Request interceptor to add auth token if available
 http.interceptors.request.use(
     (config) => {
         const token = localStorage.getItem('token');
@@ -21,11 +19,9 @@ http.interceptors.request.use(
     (error) => Promise.reject(error)
 );
 
-// Response interceptor for error handling
 http.interceptors.response.use(
     (response) => response,
     (error) => {
-        // Optionally handle global errors here
         return Promise.reject(error)
     }
 );
