3c32e424c5ae8591819c422775dc09c549b26d25
peer hammad <peerhammad007@gmail.com>
2026-01-04 18:55:02 +0530

    used the useAuth hook in privateroute

--- client/src/components/PrivateRoute.tsx
+++ client/src/components/PrivateRoute.tsx
@@ -1,11 +1,9 @@
-import React from 'react';
-import { Navigate, Outlet } from 'react-router-dom';
-import { useSelector } from 'react-redux';
-import { RootState } from '../store';
+import { Navigate, Outlet } from "react-router-dom";
+import useAuth from "../hooks/useAuth";
 
-const PrivateRoute: React.FC = () => {
-  const token = useSelector((state: RootState) => state.auth.token);
-  return token ? <Outlet /> : <Navigate to="/login" replace />;
-};
+const PrivateRoute = () => {
+    const { token } = useAuth();
+    return token ? <Outlet /> : <Navigate to='/login' replace />
+}
 
 export default PrivateRoute;
